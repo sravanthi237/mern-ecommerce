527b112d571c51f8f508b71b1193618e45970105
sravanthi237 <sravanthimanthena4@gmail.com>
2026-04-16 10:01:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+
+export default function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+      
+    </div>
+  )
+}
